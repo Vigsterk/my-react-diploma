b6a08743186fbf3310f649f5bb48cb51b214a1fb
Vigsterk <31843088+Vigsterk@users.noreply.github.com>
2018-07-30 17:43:52 +0300

    Add catalogue items in simple arrays

---
+++ src/components/Catalogue/CatalogueItemsData.js
@@ -0,0 +1,42 @@
+const sidebarColorData = [
+  {
+    color: "beige",
+    colorName: "Бежевый",
+    colorid: 1
+  },
+  {
+    color: "whitesnake",
+    colorName: "Белый",
+    colorId: 2
+  },
+  {
+    color: "shocking-blue",
+    colorName: "Голубой",
+    colorId: 3
+  },
+  {
+    color: "yellow",
+    colorName: "Жёлтый",
+    colorId: 4
+  },
+  {
+    color: "king-crimson",
+    colorName: "Алый",
+    colorId: 5
+  },
+  {
+    color: "deep-purple",
+    colorName: "Фиолетовый",
+    colorId: 6
+  },
+  {
+    color: "black-sabbath",
+    colorName: "Чёрный",
+    colorId: 7
+  }
+]
+
+const sidebarOccasionData = ["Офис", "Вечеринка", "Свадьба", "Спорт", "Путешествие", "Свидание", "Дома", "Произвести впечатление"]
+const sidebarDivisionData = ["Балетки", "Босоножки и сандалии", "Ботильоны", "Ботинки", "Ботфорты", "Галоши", "Тапочки", "Туфли", "Сапоги"]
+
+export { sidebarColorData, sidebarOccasionData, sidebarDivisionData }
